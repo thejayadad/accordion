86ee3a20605ad416676c7ea0053135bacb79ef12
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-10-07 07:55:52 -0400

    update

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let accordion = document.getElementsByClassName("box")
+for (let i = 0; i < accordion.length; i++){
+    accordion[i].addEventListener("click", function(){
+        this.classList.toggle("active")
+    })
+}
